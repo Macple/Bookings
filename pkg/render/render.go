e1ea0eef05e881d12727125a637eca8ca349340d
Maciej Plewko <maciej.plewko@gmail.com>
2021-04-05 18:59:51 +0200

    [ADD] Added CSRF token to rendered pages

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/justinas/nosurf"
+
 	"github.com/Macple/Bookings/pkg/config"
 	"github.com/Macple/Bookings/pkg/models"
 )
@@ -20,13 +22,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
-
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders templates using html template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
@@ -35,18 +37,15 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 		tc, _ = CreateTemplateCache()
 	}
 
-	//fmt.Println("Length of TC:", len(tc))
-
 	t, ok := tc[tmpl]
-	//fmt.Println("OK:", ok)
-	//fmt.Println("Found t:", t)
+
 	if !ok {
 		log.Fatal("Could not get a template from template cache")
 	}
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 
