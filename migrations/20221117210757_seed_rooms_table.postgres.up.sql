c07d173fcc812fa03d482f20e00c557550f45095
Maciej Plewko <maciej.plewko@gmail.com>
2022-11-17 22:13:32 +0100

    [ADD] SODA migration for postgres DB seeding needed rooms and restrictions table with data

---
+++ migrations/20221117210757_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quarters','2022-11-11 00:00:00','2022-11-11 00:00:00'),
+	 ('Majors''s Suite','2022-11-15 00:00:00','2022-11-15 00:00:00');
---
+++ migrations/20221117211059_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2022-11-11 00:00:00','2022-11-11 00:00:00'),
+	 ('Owner Block','2022-11-15 00:00:00','2022-11-15 00:00:00');
